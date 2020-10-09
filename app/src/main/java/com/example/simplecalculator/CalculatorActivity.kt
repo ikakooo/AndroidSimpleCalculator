15326b8d563c3d33f8dd618e2a2233ced2319090
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-10-10 01:55:16 +0400

    save

--- app/src/main/java/com/example/simplecalculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/simplecalculator/CalculatorActivity.kt
@@ -1,11 +1,42 @@
 package com.example.simplecalculator
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat.startActivity
+import kotlinx.android.synthetic.main.activity_calculator.*
+import kotlinx.android.synthetic.main.activity_calculator.view.*
 
 class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+        init()
+    }
+
+    private fun init() {
+
+
+    }
+
+
+    fun Button.customOnClickAction() {
+        if (firstNumber.text.toString().isNotEmpty() && secondNumber.text.toString().isNotEmpty()) {
+            setOnClickListener {
+                runResultActivity()
+            }
+        }
+
+
+    }
+
+    fun runResultActivity() {
+        val intent = Intent(this@CalculatorActivity, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
     }
 }
